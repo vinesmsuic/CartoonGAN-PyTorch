805e5d96a0ad95c6a3173c22a53166d19ae39922
vinesmsuic <wingfku2@gmail.com>
2022-02-11 20:07:46 +0800

    Update discriminator_model.py

--- discriminator_model.py
+++ discriminator_model.py
@@ -43,7 +43,9 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         x = self.model(x)
-        return torch.sigmoid(x)
+        return x
+        #No sigmoid for LSGAN adv loss
+        #return torch.sigmoid(x)
 
 
 
